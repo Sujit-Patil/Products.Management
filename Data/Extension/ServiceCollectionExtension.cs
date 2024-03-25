59014c230d5df8c0a3fe3a44820e4a7b224783b7
Sujit-Patil <131465018+Sujit-Patil@users.noreply.github.com>
2024-03-25 23:33:25 +0530

    Added common class library to manage Dbcontext and Repositories

---
+++ Data/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductsManagement.Data.DBContext;
+
+namespace YourNamespace.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
---
+++ Data/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProductsManagement.Data.DBContext;
+using ProductsManagement.Data.Interfaces;
+using ProductsManagement.Data.Models;
+
+namespace ProductsManagement.Data.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext Context) : base(Context)
+        {
+        }
+    }
+}
